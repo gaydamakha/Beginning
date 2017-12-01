f6f914cfa62613e82ad877637bbad619fbf97a85
root <root@node-game.tyxbyajeavzezpio0dtmfyvxoe.ax.internal.cloudapp.net>
2017-12-01 19:03:13 +0000

    ...

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ serv.listen(80);
 console.log('Сервер запущен.')
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		number:"" + Math.floor(10 * Math.random())
+	}
+	return self;
+}
 
 var io = require('socket.io')(serv,{});
 
@@ -21,20 +32,24 @@ io.sockets.on('connection', function(socket){
 	socket.x = 0;
 	socket.y = 0;
 	SOCKET_LIST[socket.id] = socket;
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
 	console.log('Пользователь ' + socket.id + ' подключился.');
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
 		console.log('Пользователь ' + socket.id + ' отключился.')
 	});
 });
 
 setInterval(function(){
 	var pack=[];
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
+	for(var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
 		pack.push({
-			x:socket.x++,
-			y:socket.y++
+			x:player.x++,
+			y:player.y++,
+			number:player.number
 		});
 	}
 	for (var i in SOCKET_LIST){
