b643962fe2da476dc77e1e534513b2a3c2c8077b
root <root@node-game.tyxbyajeavzezpio0dtmfyvxoe.ax.internal.cloudapp.net>
2017-12-02 11:52:46 +0000

    KeyPress

--- app.js
+++ app.js
@@ -1,44 +1,83 @@
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
+var server = require('http').Server(app);
 
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/',express.static(__dirname + '/client'));
 
-serv.listen(80);
-
+server.listen(80);
 
 console.log('Сервер запущен.')
 
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
 
+
 var Player = function(id){
 	var self = {
 		x:250,
 		y:250,
 		id:id,
-		number:"" + Math.floor(10 * Math.random())
-	}
+		number:"" + Math.floor(10 * Math.random()),
+		pressingRight:false,
+		pressingLeft:false,
+		pressingUp:false,
+		pressingDown:false,
+		maxSpd:10,
+	};
+	self.updatePosition = function(){
+
+		a=1;
+		if (self.pressingRight && self.pressingUp ||
+			self.pressingRight && self.pressingDown ||
+			self.pressingLeft  && self.pressingUp ||
+			self.pressingLeft  && self.pressingDown ||
+			self.pressingUp    && self.pressingRight ||
+			self.pressingDown  && self.pressingRight ||
+			self.pressingUp    && self.pressingLeft ||
+			self.pressingDown  && self.pressingLeft)
+			a=Math.pow(2,1/2)/2;
+
+		if (self.pressingRight)
+			self.x+=self.maxSpd*a;
+		if (self.pressingLeft)
+			self.x-=self.maxSpd*a;
+		if (self.pressingUp)
+			self.y-=self.maxSpd*a;
+		if (self.pressingDown)
+			self.y+=self.maxSpd*a;
+	};
 	return self;
 }
 
-var io = require('socket.io')(serv,{});
+var io = require('socket.io')(server,{});
 
 io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
 	SOCKET_LIST[socket.id] = socket;
+
 	var player = Player(socket.id);
 	PLAYER_LIST[socket.id] = player;
+
 	console.log('Пользователь ' + socket.id + ' подключился.');
+
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
 		delete PLAYER_LIST[socket.id];
-		console.log('Пользователь ' + socket.id + ' отключился.')
+		console.log('Пользователь ' + socket.id + ' отключился.');
+	});
+
+	socket.on('keyPress',function(data){
+		if (data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
 	});
 });
 
@@ -46,9 +85,10 @@ setInterval(function(){
 	var pack=[];
 	for(var i in PLAYER_LIST){
 		var player = PLAYER_LIST[i];
+		player.updatePosition();
 		pack.push({
-			x:player.x++,
-			y:player.y++,
+			x:player.x,
+			y:player.y,
 			number:player.number
 		});
 	}
--- client/js/common.js
+++ client/js/common.js
@@ -16,9 +16,29 @@ $(document).ready(function(){
 	socket.on('newPosition',function(data){
 		areaCtx.clearRect(0,0,area.width,area.height);
 		for (var i=0; i<data.length;i++)
-			areaCtx.fillText('LEHA',data[i].x,data[i].y);
+			areaCtx.fillText(data[i].number,data[i].x,data[i].y);
 	});
 
+	document.onkeydown = function(event){
+		if (event.keyCode === 68)
+			socket.emit('keyPress',{inputId:'right',state:true});
+		else if (event.keyCode === 83)
+			socket.emit('keyPress',{inputId:'down',state:true});
+		else if (event.keyCode === 65)
+			socket.emit('keyPress',{inputId:'left',state:true});
+		else if (event.keyCode === 87)
+			socket.emit('keyPress',{inputId:'up',state:true});
+	};
+	document.onkeyup = function(event){
+		if (event.keyCode === 68)
+			socket.emit('keyPress',{inputId:'right',state:false});
+		else if (event.keyCode === 83)
+			socket.emit('keyPress',{inputId:'down',state:false});
+		else if (event.keyCode === 65)
+			socket.emit('keyPress',{inputId:'left',state:false});
+		else if (event.keyCode === 87)
+			socket.emit('keyPress',{inputId:'up',state:false});
+	};
 });
 
 $(function() {
